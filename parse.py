2e0b05901f6b9a82dcf0f1eaac867473be1bffe6
DC <12854+danchoi@users.noreply.github.com>
2023-03-25 15:08:38 -0400

    get search-result

--- parse.py
+++ parse.py
@@ -8,3 +8,13 @@ print(soup.title.string)
 
 
 
+
+# find search-result-group, then class='contents', then inside, the
+# div.search-result is the post, repeated
+
+# title is in <header class="search-result-header">
+
+
+result = soup.find_all('div', class_="search-result")
+
+print(result)
