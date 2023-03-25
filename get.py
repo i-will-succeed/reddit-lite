f2896b1f57d779b366f0cd6c84dabf1a1982044a
DC <12854+danchoi@users.noreply.github.com>
2023-03-25 18:42:57 -0400

    get.py successfully searches reddit with requests lib

---
+++ get.py
@@ -0,0 +1,42 @@
+import requests
+import sys
+from bs4 import BeautifulSoup
+import subprocess
+
+reddit_name = sys.argv[1]
+reddit_search = sys.argv[2].replace(' ', '+')
+reddit_link = f"https://old.reddit.com/r/{reddit_name}/search?q={reddit_search}&restrict_sr=on"
+
+headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0'
+}
+response = requests.get(reddit_link, headers=headers)
+
+
+html_file = response.text
+soup = BeautifulSoup(html_file, 'html.parser')
+
+# Print Title
+print(soup.title.string)
+
+
+
+
+# find search-result-group, then class='contents', then inside, the
+# div.search-result is the post, repeated
+
+# title is in <header class="search-result-header">
+
+for item in soup.find_all('div', class_="search-result"):
+    print('-----------------------------')
+    title = item.find_all(class_="search-title")[0].string
+ 
+    score = item.find_all(class_="search-score")[0].string
+    time = item.find_all('time')[0].string
+    author = item.find_all(class_='author')[0].string
+    comments = item.find_all(class_='search-comments')[0].string
+    link = item.find_all(class_="search-title")[0]['href']
+ 
+
+    print(f'Title: {title.strip()}\n\nAuthor: {author.strip()}\nComments: {comments.strip()}\nScore: {score.strip()}\nTime: {time.strip()}\n\nLink: {link}\n')
+
